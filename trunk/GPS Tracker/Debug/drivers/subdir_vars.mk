8de1fa00476af207cb8299f1ccd63e24308b62f3
agrafi@gmail.com <agrafi@gmail.com@0e88301b-3124-5493-302d-d485f6abb421>
2012-03-31 12:24:02 +0000

    Code composer integration

---
+++ trunk/GPS Tracker/Debug/drivers/subdir_vars.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../drivers/gps.c \
+../drivers/timer.c \
+../drivers/uart.c 
+
+OBJS += \
+./drivers/gps.obj \
+./drivers/timer.obj \
+./drivers/uart.obj 
+
+C_DEPS += \
+./drivers/gps.pp \
+./drivers/timer.pp \
+./drivers/uart.pp 
+
+C_DEPS__QUOTED += \
+"drivers\gps.pp" \
+"drivers\timer.pp" \
+"drivers\uart.pp" 
+
+OBJS__QUOTED += \
+"drivers\gps.obj" \
+"drivers\timer.obj" \
+"drivers\uart.obj" 
+
+C_SRCS__QUOTED += \
+"../drivers/gps.c" \
+"../drivers/timer.c" \
+"../drivers/uart.c" 
+
+
---
+++ trunk/GPS Tracker/Debug/subdir_rules.mk
@@ -0,0 +1,13 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+main.obj: ../main.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: MSP430 Compiler'
+	"D:/ti/ccsv5/tools/compiler/msp430/bin/cl430" -vmspx --abi=coffabi -O0 -g --include_path="D:/ti/ccsv5/ccs_base/msp430/include" --include_path="C:/Users/Aviv/workspace_v5_1/GPS Tracker" --include_path="C:/Users/Aviv/workspace_v5_1/GPS Tracker/drivers/fs/src" --include_path="D:/ti/ccsv5/tools/compiler/msp430/include" --define=__MSP430F2618__ --diag_warning=225 --display_error_number --printf_support=minimal --preproc_with_compile --preproc_dependency="main.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
